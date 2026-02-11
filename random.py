0e096aa0dfde4fd7513a5c8ef66e61c26830b37a
1234-000 <wilsonm68@nycstudents.net>
2026-02-11 09:09:45 -0500

    ww

--- random.py
+++ random.py
@@ -1,4 +1,4 @@
 import random
-numbers = [1,2,3,4,5,6,7,8,9,10]
-random_number = random.choice(numbers)
-print(random_number)
+
+numbers = ["1","2","3","4","5","6","7","8","9","10"]
+print(random.choice(numbers))
